9083eac75684107881de14f5c6d78a9f1bef4ba6
AlejandraJaraQ <140223798+AlejandraJaraQ@users.noreply.github.com>
2023-09-15 09:52:58 -0500

    Add files via upload

---
+++ S08/src/paquete1/clase2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete1;
+
+/**
+ *
+ * @author aleja
+ */
+public class clase2 {
+    private int a;
+    public int c;
+
+
+}
